4b7f0b422840939ab8ad7149cedd1f2931f589ab
Bruno Henrique <47230010+BrunoDorea@users.noreply.github.com>
2022-10-17 09:32:05 -0300

    Fatorial Desajeitado

--- DesafioIntermediario2/main.js
+++ DesafioIntermediario2/main.js
@@ -11,5 +11,31 @@ let lines = gets().split("\n")
 let n = parseInt(lines.shift())
 
 if (n <= 2) { 
- print(n)
+  n * 1
+  print(n)
+} else {
+  let res = 0
+  let cnt = 1
+  let tmp = n
+  n--
+  while (n > 0) {
+  tmp *= n
+  n--
+  if(n != 0){
+    tmp /= n;
+    n--
+  }
+  tmp += n
+  n--
+
+  res += tmp
+
+  tmp = -n
+  if (n == 1 ){
+    res += tmp
+  }   
+
+  n--
+}
+  print(res.toFixed(0))
 } 
